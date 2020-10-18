3b77a301a284f771decb656bbd33d441376116b9
SongZihuan <jimsong2010@163.com>
2020-10-18 17:26:18 +0800

    feat: debug模式下关闭IO缓冲

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 int main(int argc, char *argv[]) {
     Inter *inter = NULL;
 
+#ifdef DEBUG
+    setbuf(stdout, NULL);  // debug模式下关闭缓冲区以尽快输出信息便于debug
+    setbuf(stderr, NULL);
+    setbuf(stdin, NULL);
+#endif
+
     atexit(freeArgs);
     signal(SIGINT, SIG_IGN);
     if (setjmp(memVirtualMath_Env) == -1){
